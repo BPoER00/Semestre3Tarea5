0a78e8495d4aab536112fa95748ff24e9136382b
Renato Granados <lrenatogo1t@gmail.com>
2021-03-02 10:39:59 +0100

    Terminando interfaz de registros

--- src/application/controlador/RegistroControlador.java
+++ src/application/controlador/RegistroControlador.java
@@ -9,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
@@ -37,6 +38,15 @@ public class RegistroControlador {
 	private Button btnSalir;
 
 	@FXML
+	private Label labelErrorCarnet;
+
+	@FXML
+	private Label labelErrorNombre;
+
+	@FXML
+	private Label labelErrorCurso;
+
+	@FXML
 	void clickGuardar(ActionEvent event) {
 		System.out.println("test");
 	}
@@ -59,13 +69,32 @@ public class RegistroControlador {
 	@FXML
 	void initialize() {
 		insertarDatosAlComboBox();
+		labelErrorCarnet.setVisible(false);
+		labelErrorNombre.setVisible(false);
+		labelErrorCurso.setVisible(false);
 	}
 
+	/**
+	 * Inserta una lista de cursos disponbiles para los estudiantes
+	 */
 	private void insertarDatosAlComboBox() {
 		ObservableList<String> items = FXCollections.observableArrayList();
 
-		// Aquí voy a poner todos los cursos de Sistemas Bryan
-		items.addAll("item 1", "item 2", "item 3");
+		// Agregamos los cursos|
+		items.addAll("Administración de tecnologías de información", "Algebra lineal", "Algoritmos",
+				"Análisis de sistemas I", "Análisis de sistemas II", "Arquitectura de computadoras I",
+				"Arquitectura de computadoras II", "Aseguramiento de la calidad del software", "Autómatas y lenguajes",
+				"Base de datos I", "Base de datos II", "Cálculo I", "Cálculo II", "Compiladores", "Contabilidad I",
+				"Contabilidad II", "Derecho Informático", "Desarrollo Humano", "Desarrollo Web",
+				"Electrónica analógica", "Electrónica digital", "Emprendedores de negocios", "Estadística I",
+				"Estadística II", "Ética profesional", "Física I", "Física II", "Inteligencia artificial",
+				"Inteniería de software", "Introducción a los sistemas", "Investigación de operaciones",
+				"Lógica de sistemas", "Matemática Discreta", "Metodo de la Investigación", "Métodos numéricos",
+				"Microeconomía", "Precálculo", "Proceso Administrativo", "Programación I", "Programación II",
+				"Programación III", "Proyecto de graduación I", "Proyecto de graduación II", "Redes de computadoras I",
+				"Redes de computadoras II", "Seguridad y auditoria de Sistemas",
+				"Seminarios de tecnologías de información", "Sistemas operativos", "Sistemas operativos II",
+				"Telecomunicaciones");
 
 		comboCurso.setItems(items);
 		comboCurso.setEditable(false);
