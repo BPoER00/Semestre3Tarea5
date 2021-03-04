be7bce0818224611525fcce96bb330c5d946a126
BPoER00 <78948945+BPoER00@users.noreply.github.com>
2021-03-04 10:09:35 -0600

    Consultas de Guardar y eliminar

--- src/Conexion/ConexionDB.java
+++ src/Conexion/ConexionDB.java
@@ -2,18 +2,32 @@
 package Conexion;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import javax.swing.JOptionPane;
-
-
+ 
 public class ConexionDB {
     
-    public static void Connect(){
+    private String url = "jdbc:sqlite:DatosTarea5.db";
+    private Connection datos = null;
+    
+    public Connection conexion(){
+        // SQLite connection string
         Connection conn = null;
-        try{
-            String url = "jdbc:sqlite:|";
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(null, "Advertencia","Error: "+e, JOptionPane.ERROR_MESSAGE);
+        try {
+            String url = "jdbc:sqlite:DatosTarea5.db";
+            
+            conn = DriverManager.getConnection(url);
+            
+            if(conn == null){
+                System.out.println("no hay conexion");
+            }else{
+                System.out.println("Conexion exitosa");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+        return conn;
+    
     }
     
 }
